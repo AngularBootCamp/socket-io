b641930acb3171531acfde694a0445950c068fff
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-11-10 11:15:17 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -5,7 +6,9 @@ import { SocketService } from './socket.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [AsyncPipe]
 })
 export class AppComponent {
   visitCount: Observable<string>;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 // https://github.com/stackblitz/core/issues/2366
 import 'zone.js'; // Avoid error in StackBlitz for Angular polyfill
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const config: SocketIoConfig = {
+  url: 'http://localhost:8085',
+  options: {}
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(SocketIoModule.forRoot(config))]
+}).catch(err => console.error(err));
