bc0b1c80e66a4550c7c7e6e064f8d885d3a9e5c7
Zachary Kipping <zachary.kipping@oasisdigital.com>
2024-07-29 11:06:23 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 // https://github.com/stackblitz/core/issues/2366
 import 'zone.js'; // Avoid error in StackBlitz for Angular polyfill
 
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-const config: SocketIoConfig = {
-  url: 'http://localhost:8085',
-  options: {}
-};
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(SocketIoModule.forRoot(config))]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error(err)
+);
